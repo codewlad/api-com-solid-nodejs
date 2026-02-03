f1174fc66f4e6fc44e304fea876d908bf747e23e
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2026-02-03 19:10:59 -0300

    feat: Prism added

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
 import fastify from 'fastify'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+
+const connectionString = `${process.env.DATABASE_URL}`
 
 export const app = fastify()
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+prisma.user.create({
+  data: {
+    name: 'Wladimir Ferreira',
+    email: 'wladimir.ferreira@example.com',
+  },
+})
