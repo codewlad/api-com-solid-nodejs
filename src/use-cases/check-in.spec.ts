312f137422b49df3483a1344dbf85b59441dc12a
codewlad <codewlad@gmail.com>
2026-02-19 16:59:25 -0300

    feat: update Prisma dependencies and add authentication use case
    - Bump Prisma and related packages to version 7.4.0
    - Implement user authentication functionality with new `authenticate` controller and use case
    - Add error handling for invalid credentials
    - Create factories for use cases to simplify instantiation
    - Introduce new repositories for check-ins and gyms with in-memory implementations
    - Add check-in use case with distance validation and maximum check-in constraints
    - Implement user profile retrieval functionality
    - Add utility for calculating distance between coordinates
    - Enhance tests for new features and ensure coverage for edge cases

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,116 @@
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
+import { CheckInUseCase } from './check-in'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client-runtime-utils'
+import { MaxNumberOfCheckInsError } from './errors/max-numbers-of-check-ins-error'
+import { MaxDistanceError } from './errors/max-distance-error'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
+let sut: CheckInUseCase
+
+describe('Check in Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-23.665697),
+      longitude: new Decimal(-46.5066368),
+    })
+
+    await gymsRepository.create({
+      id: 'gym-01',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-23.665697),
+      longitude: new Decimal(-46.5066368),
+    })
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.665697,
+      userLongitude: -46.5066368,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.665697,
+      userLongitude: -46.5066368,
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+        userLatitude: -23.665697,
+        userLongitude: -46.5066368,
+      }),
+    ).rejects.toBeInstanceOf(MaxNumberOfCheckInsError)
+  })
+
+  it('should not be able to check in twice in the different day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.665697,
+      userLongitude: -46.5066368,
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.665697,
+      userLongitude: -46.5066368,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-23.6886022),
+      longitude: new Decimal(-46.5048811),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -23.665697,
+        userLongitude: -46.5066368,
+      }),
+    ).rejects.toBeInstanceOf(MaxDistanceError)
+  })
+})
